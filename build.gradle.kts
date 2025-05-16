19d6c4b48745b898d097b137659a931398d855b1
Wzp-2008 <minecraftwzpmc@163.com>
2025-05-16 10:46:13 +0800

    build(deps): update deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import java.net.URI
 plugins {
 	java
 	id("org.springframework.boot") version "3.3.4"
-	id("io.spring.dependency-management") version "1.1.6"
+	id("io.spring.dependency-management") version "1.1.7"
 	id("org.graalvm.buildtools.native") version "0.10.3"
 	id("org.asciidoctor.jvm.convert") version "3.3.2"
 }
@@ -40,18 +40,18 @@ dependencies {
 	}
 	implementation("org.springframework.shell:spring-shell-starter")
 	// https://mvnrepository.com/artifact/com.mybatis-flex/mybatis-flex-spring-boot3-starter
-	implementation("com.mybatis-flex:mybatis-flex-spring-boot3-starter:1.9.7")
-	annotationProcessor("com.mybatis-flex:mybatis-flex-processor:1.9.7")
+	implementation("com.mybatis-flex:mybatis-flex-spring-boot3-starter:1.10.9")
+	annotationProcessor("com.mybatis-flex:mybatis-flex-processor:1.10.9")
 	compileOnly("org.mybatis.spring.native:mybatis-spring-native-extensions:0.1.0-SNAPSHOT")
 	compileOnly("org.mybatis.spring.native:mybatis-spring-native-core:0.1.0-SNAPSHOT")
 	// https://mvnrepository.com/artifact/com.alibaba.fastjson2/fastjson2
 	implementation("com.alibaba.fastjson2:fastjson2:2.0.46")
 	// https://mvnrepository.com/artifact/com.alibaba.fastjson2/fastjson2-extension
-	implementation("com.alibaba.fastjson2:fastjson2-extension:2.0.46")
+	implementation("com.alibaba.fastjson2:fastjson2-extension:2.0.57")
 	// https://mvnrepository.com/artifact/com.alibaba.fastjson2/fastjson2-extension-spring6
-	implementation("com.alibaba.fastjson2:fastjson2-extension-spring6:2.0.46")
+	implementation("com.alibaba.fastjson2:fastjson2-extension-spring6:2.0.57")
 	// https://mvnrepository.com/artifact/com.alibaba/druid-spring-boot-starter
-	implementation("com.alibaba:druid-spring-boot-starter:1.2.23")
+	implementation("com.alibaba:druid-spring-boot-starter:1.2.24")
 	// https://mvnrepository.com/artifact/com.auth0/java-jwt
 	implementation("com.auth0:java-jwt:4.4.0")
 	// https://mvnrepository.com/artifact/commons-codec/commons-codec
