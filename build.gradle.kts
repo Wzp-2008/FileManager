5253b2364a81749ce7f18513d62db1fb759cae6d
wzp <minecraftwzpmc@163.com>
2026-03-03 00:00:55 +0800

    deps: update deps version

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 	java
 	id("org.springframework.boot") version "3.3.4"
 	id("io.spring.dependency-management") version "1.1.7"
-	id("org.graalvm.buildtools.native") version "0.10.3"
+	id("org.graalvm.buildtools.native") version "0.11.4"
 	id("org.asciidoctor.jvm.convert") version "4.0.5"
 }
 
@@ -32,8 +32,8 @@ repositories {
 
 extra["snippetsDir"] = file("build/generated-snippets")
 extra["springShellVersion"] = "3.3.3"
-val fastjsonVersion = "2.0.57"
-val mybatisFlexVersion = "1.10.9"
+val fastjsonVersion = "2.0.61"
+val mybatisFlexVersion = "1.11.6"
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-data-redis")
@@ -45,25 +45,22 @@ dependencies {
 	// https://mvnrepository.com/artifact/com.mybatis-flex/mybatis-flex-spring-boot3-starter
 	implementation("com.mybatis-flex:mybatis-flex-spring-boot3-starter:${mybatisFlexVersion}")
 	annotationProcessor("com.mybatis-flex:mybatis-flex-processor:${mybatisFlexVersion}")
-	/* compileOnly("org.mybatis.spring.native:mybatis-spring-native-extensions:${mybatisSpringNativeVersion}")
-	compileOnly("org.mybatis.spring.native:mybatis-spring-native-core:${mybatisSpringNativeVersion}")*/
 	// https://mvnrepository.com/artifact/com.alibaba.fastjson2/fastjson2
 	implementation("com.alibaba.fastjson2:fastjson2:${fastjsonVersion}")
-	implementation("com.alibaba.fastjson2:fastjson2-codegen:${fastjsonVersion}")
 	// https://mvnrepository.com/artifact/com.alibaba.fastjson2/fastjson2-extension
 	implementation("com.alibaba.fastjson2:fastjson2-extension:${fastjsonVersion}")
 	// https://mvnrepository.com/artifact/com.alibaba.fastjson2/fastjson2-extension-spring6
 	implementation("com.alibaba.fastjson2:fastjson2-extension-spring6:${fastjsonVersion}")
 	// https://mvnrepository.com/artifact/com.alibaba/druid-spring-boot-starter
-	implementation("com.alibaba:druid-spring-boot-starter:1.2.24")
+	implementation("com.alibaba:druid-spring-boot-starter:1.2.27")
 	// https://mvnrepository.com/artifact/com.auth0/java-jwt
-	implementation("com.auth0:java-jwt:4.4.0")
+	implementation("com.auth0:java-jwt:4.5.1")
 	// https://mvnrepository.com/artifact/commons-codec/commons-codec
-	implementation("commons-codec:commons-codec:1.17.1")
+	implementation("commons-codec:commons-codec:1.21.0")
 	// https://mvnrepository.com/artifact/org.apache.tika/tika-core
-	implementation("org.apache.tika:tika-core:3.0.0-BETA2")
+	implementation("org.apache.tika:tika-core:3.2.3")
 	// https://mvnrepository.com/artifact/com.mysql/mysql-connector-j
-	implementation("com.mysql:mysql-connector-j:9.0.0")
+	implementation("com.mysql:mysql-connector-j:9.6.0")
 	compileOnly("org.projectlombok:lombok")
 	/*developmentOnly("org.springframework.boot:spring-boot-devtools")*/
 	annotationProcessor("org.projectlombok:lombok")
