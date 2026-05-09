05ed49cfdda68bd30d5510ec7edf0c95e25e71ca
wzp <minecraftwzpmc@163.com>
2026-05-09 08:23:47 +0800

    deps: add nuxt ui

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import App from './App.vue'
+import {createApp} from 'vue'
+import {createPinia} from 'pinia'
+import ui from '@nuxt/ui/vue-plugin'
+import App from '@/App.vue'
+import '@/style/main.css'
 
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(ui)
 
 app.mount('#app')
